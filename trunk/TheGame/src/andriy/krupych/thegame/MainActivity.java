d195eb8e0d03d076608a4638cf3fc3104389820a
andriy.krupych@gmail.com <andriy.krupych@gmail.com@688a51b8-834e-ff09-ba21-0ee52527799d>
2014-10-29 00:19:33 +0000

    graphics and textures

--- trunk/TheGame/src/andriy/krupych/thegame/MainActivity.java
+++ trunk/TheGame/src/andriy/krupych/thegame/MainActivity.java
@@ -14,6 +14,13 @@ import org.andengine.engine.options.ScreenOrientation;
 import org.andengine.engine.options.WakeLockOptions;
 import org.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
 import org.andengine.entity.scene.Scene;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
+import org.andengine.opengl.texture.atlas.bitmap.BuildableBitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.source.IBitmapTextureAtlasSource;
+import org.andengine.opengl.texture.atlas.buildable.builder.BlackPawnTextureAtlasBuilder;
+import org.andengine.opengl.texture.atlas.buildable.builder.ITextureAtlasBuilder.TextureAtlasBuilderException;
+import org.andengine.opengl.texture.region.TextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
 
 public class MainActivity extends BaseGameActivity {
@@ -22,6 +29,9 @@ public class MainActivity extends BaseGameActivity {
 	private Scene mScene;
 	private Sound mSound;
 	private Music mMusic;
+	private TextureRegion mOgreTextureRegion;
+	private TextureRegion mTrollTextureRegion;
+	private TextureRegion mGruntTextureRegion;
 	
 	@Override
 	public Engine onCreateEngine(EngineOptions pEngineOptions) {
@@ -46,6 +56,23 @@ public class MainActivity extends BaseGameActivity {
 			mMusic = MusicFactory.createMusicFromAsset(getMusicManager(), this, "music.mp3");
 			mSound = SoundFactory.createSoundFromAsset(getSoundManager(), this, "sound.mp3");
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
+		BuildableBitmapTextureAtlas atlas = new BuildableBitmapTextureAtlas(
+				getTextureManager(), 200, 200);
+//		BitmapTextureAtlas atlas = new BitmapTextureAtlas(getTextureManager(), 124, 46);
+		mOgreTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+				atlas, this, "ogre.png");
+		mTrollTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+				atlas, this, "troll.png");
+		mGruntTextureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+				atlas, this, "grunt.png");
+		try {
+			atlas.build(new BlackPawnTextureAtlasBuilder<IBitmapTextureAtlasSource,
+					BitmapTextureAtlas>(1, 1, 1));
+			atlas.load();
+		} catch (TextureAtlasBuilderException e) {
 			e.printStackTrace();
 		}
 		pOnCreateResourcesCallback.onCreateResourcesFinished();
